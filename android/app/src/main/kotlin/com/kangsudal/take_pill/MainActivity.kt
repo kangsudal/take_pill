0be41f967a9844ec78b225b94ccd652d2eeb2435
kangsudal_notebook <kangsudal@gmail.com>
2023-04-25 04:22:45 +0900

    버튼을 누르면 약을 먹은 시간을 저장

---
+++ android/app/src/main/kotlin/com/kangsudal/take_pill/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kangsudal.take_pill
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('myBox');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '약 챙겨먹기',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: '약챙겨 드세요!!'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    String korWeekDay = '';
+    List<String> korWeekDays = ['월', '화', '수', '목', '금', '토', '일'];
+    korWeekDay = korWeekDays[DateTime.now().weekday - 1];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ValueListenableBuilder<Box>(
+          valueListenable: Hive.box('myBox').listenable(),
+          builder: (context, box, widget) {
+            String timeStampBreakfast =
+                box.get('timeStampBreakfast', defaultValue: '');
+            String timeStampLunch = box.get('timeStampLunch', defaultValue: '');
+            String timeStampDinner =
+                box.get('timeStampDinner', defaultValue: '');
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                    "${DateTime.now().year}/${DateTime.now().month}/${DateTime.now().day}($korWeekDay)"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {
+                        box.put('timeStampBreakfast', DateTime.now().toString(),);
+                      },
+                      child: Text('아침약'),
+                    ),
+                    Text(
+                      timeStampBreakfast,
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                    if (timeStampBreakfast != '')
+                      IconButton(
+                        onPressed: () {
+                          box.delete('timeStampBreakfast');
+                        },
+                        icon: Icon(
+                          Icons.remove_circle,
+                          color: Colors.blueAccent,
+                          size: 20,
+                        ),
+                      ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {
+                        box.put('timeStampLunch', DateTime.now().toString(),);
+                      },
+                      child: Text('점심약'),
+                    ),
+                    Text(
+                      timeStampLunch,
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                    if (timeStampLunch != '')
+                      IconButton(
+                        onPressed: () {
+                          box.delete('timeStampLunch');
+                        },
+                        icon: Icon(
+                          Icons.remove_circle,
+                          color: Colors.blueAccent,
+                          size: 20,
+                        ),
+                      ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {
+                        box.put('timeStampDinner', DateTime.now().toString(),);
+                      },
+                      child: Text('저녁약'),
+                    ),
+                    Text(
+                      timeStampDinner,
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                    if (timeStampDinner != '')
+                      IconButton(
+                        onPressed: () {
+                          box.delete('timeStampDinner');
+                        },
+                        icon: Icon(
+                          Icons.remove_circle,
+                          color: Colors.blueAccent,
+                          size: 20,
+                        ),
+                      ),
+                  ],
+                ),
+              ],
+            );
+          }),
+    );
+  }
+}
